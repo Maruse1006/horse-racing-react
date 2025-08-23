7050327cc9c64052438baf006c8fa22f34d654c0
Maruse1006 <oonnwu9119@yahoo.ne.jp>
2025-08-23 22:23:59 +0900

    update

--- data/calendarData.ts
+++ data/calendarData.ts
@@ -36,9 +36,17 @@ export const calendarData = {
     { placeId: "02", round: "01", day: "12" },
     { placeId: "07", round: "第2回", day: "02", year: "2025" }, // 中京競馬場
   ],
-  "2025-07-20": [
-    { placeId: "02", round: "01", day: "12" },
-    { placeId: "07", round: "第2回", day: "02", year: "2025" }, // 中京競馬場
+   "2025-08-02": [
+    { placeId: "04", round: "第2回", day: "03", year: "2025" },
+    { placeId: "07", round: "第3回", day: "03", year: "2025" }, // 中京競馬場
+  ],
+    "2025-08-03": [
+    { placeId: "04", round: "第2回", day: "04", year: "2025" },
+    { placeId: "07", round: "第3回", day: "04", year: "2025" }, // 中京競馬場
+  ],
+  "2025-08-10": [
+    { placeId: "07", round: "第3回", day: "06", year: "2025" }, // 中京競馬場
+    { placeId: "04", round: "第2回", day: "06", year: "2025" },
   ],
 
 
@@ -47,6 +55,7 @@ export const calendarData = {
 
 export const placeData = [
   { id: "02", name: "函館競馬場" },
+  { id: "04", name: "新潟競馬場" },
   { id: "05", name: "東京競馬場" },
   { id: "06", name: "中山競馬場" },
   { id: "07", name: "中京競馬場" },
